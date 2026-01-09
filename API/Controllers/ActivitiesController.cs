b4c34c982dace6d3e78a145a4c0c1184a1369279
Wingapo <wingapo2048@gmail.com>
2026-01-10 00:15:14 +0200

    Add activities controller with GET requests

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+public class ActivitiesController(ApplicationDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+        return activity == null ? NotFound() : activity;
+    }
+}
