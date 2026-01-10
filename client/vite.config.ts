6e2b9cb407f1bdb15438cfa3362abd26e1141c54
Wingapo <wingapo2048@gmail.com>
2026-01-10 22:38:51 +0200

    Enable https on client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
+  plugins: [react(), mkcert()],
   server: {
     port: 3000,
   },
-  plugins: [react()],
 });
