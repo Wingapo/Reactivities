e1443e3a16d489b0f4ba99edf79aaf3a44c6b104
Wingapo <wingapo2048@gmail.com>
2026-01-10 21:43:15 +0200

    Add data fetch from API, fix CORS

--- API/Program.cs
+++ API/Program.cs
@@ -4,15 +4,25 @@ using Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var allowedOrigins = builder.Configuration["CLIENT_APP_URLS"]
+    ?.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries) ?? [];
+builder.Services.AddCors(options => 
+    options.AddDefaultPolicy(policy => 
+        policy
+            .WithOrigins(allowedOrigins)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()));
+
 var app = builder.Build();
 
+app.UseCors();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.MapGet("/", () => "ok");
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
-const App = () => (
-    <h1>Reactivities</h1>
-);
+import {useEffect, useState} from 'react';
+
+const App = () => {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    void fetch(`https://localhost:5001/api/activities`)
+      .then(response => response.json())
+      .then(json => setActivities(json as Activity[]));
+  }, []);
+
+  return (
+    <>
+      <h1>Reactivities</h1>
+      <ul>
+        {activities.map(activity => (
+          <li key={activity.id}>{activity.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 export default App;
